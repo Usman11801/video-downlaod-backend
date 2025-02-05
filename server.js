53eda106cb12df7f494c0561d97dade01136e268
nouman <muhammadusman@gsoftcosulting.com>
2025-02-06 01:35:46 +0500

    rout change to dlp

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.post('/preview', async (req, res) => {
   }
 
   // Command to get video info using yt-dlp
-  const command = `/usr/local/bin/yt-dlp --dump-json ${url}`;
+  const command = `yt-dlp --dump-json ${url}`;
 
   exec(command, (error, stdout, stderr) => {
     if (error) {
@@ -69,7 +69,7 @@ app.post('/download', (req, res) => {
     fs.unlinkSync(outputPath);
   }
 
-  const command = `/usr/local/bin/yt-dlp  -f "best[ext=mp4]/best" -o "${outputPath}" "${url}" --newline --progress`;
+  const command = `yt-dlp  -f "best[ext=mp4]/best" -o "${outputPath}" "${url}" --newline --progress`;
 
   console.log('Starting download...');
   
